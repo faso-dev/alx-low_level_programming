c9fe752fa2b1a5a7c499a414ea55ea3ba7bc928a
faso-dev <jeromeonadja28@gmail.com>
2022-10-13 11:53:40 -0700

    feat(hello-world): C program that prints sentence with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * prints "Programming is like building a multilingual puzzle" with puts
+ * Return: 0 for success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
