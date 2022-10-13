8fd0fc42123b5bf5d2292e6ab35347ffe999e6b6
faso-dev <jeromeonadja28@gmail.com>
2022-10-13 12:02:16 -0700

    feat(hello-world): C program that prints sentence with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * prints "with proper grammar, but the outcome is a piece of art," with printf
+ * Return: 0 for success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
