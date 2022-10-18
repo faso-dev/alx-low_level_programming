59c72c8d8831b69f140ea16378f5c7f4ded2c453
faso-dev <jeromeonadja28@gmail.com>
2022-10-18 15:58:49 -0700

    feat(func-loops): sign of number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _isalpha - Entry point
- * check if a given char is an alpha
+ * print_sign - Entry point
+ * prints the sign of the given n
  * @n: the int n
  * Return: 1 for true and 0 for false
  */
@@ -12,13 +12,12 @@ int print_sign(int n)
 	{
 		_putchar('+');
 		return (1);
-	} else if (n == 0)
+	}
+	if (n == 0)
 	{
 		_putchar(48);
 		return (0);
-	} else
-	{
-		_putchar('-');
-		return (-1);
 	}
+	_putchar('-');
+	return (-1);
 }
