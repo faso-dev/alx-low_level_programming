d59430c24b956c5626214aaac3e4ea5a051d0f40
faso-dev <jeromeonadja28@gmail.com>
2022-10-19 04:22:19 -0700

    feat(func-loops): prints all natural numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,6 +4,7 @@
 /**
  * print_to_98 - Entry point
  * prints all natural numbers from n to 98
+ * @n: the number to print from
  */
 void print_to_98(int n)
 {
@@ -12,7 +13,7 @@ void print_to_98(int n)
 	if (n <= 98)
 	{
 		for (i = n; i <= 98; i++)
-		{	
+		{
 			if (i != 98)
 			{
 				printf("%d, ", i);
