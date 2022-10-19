ab47a6f1fa5f4c8ff620e378b082a30a59dbc7c0
faso-dev <jeromeonadja28@gmail.com>
2022-10-19 04:01:32 -0700

    feat(func-loops): sum of the two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * calculate  the sum of two digits
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
