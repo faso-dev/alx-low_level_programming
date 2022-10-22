aa6ca2c3fad38d3605f897e42596fbb8b5b20c6e
faso-dev <jeromeonadja28@gmail.com>
2022-10-22 15:46:27 -0700

    feat(more-func): task #10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -4,9 +4,7 @@
 
 /**
  * main - Entry point
- * prints the numbers from 1 to 100, followed by a new line
- * but for multiples of three prints Fizz instead of the number
- * and for the multiples of five prints Buzz
+ * print triangle
  * Return: Always 0 (Success)
  */
 int main(void)
