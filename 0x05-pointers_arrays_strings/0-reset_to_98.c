54d5c729d6042a21e8a2c785bec8e4db857d8b86
faso-dev <jeromeonadja28@gmail.com>
2022-10-24 08:22:39 -0700

    feat(pointers-arrays): task #0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry file
- * updates the value of a point n it points to to 98
+ * reset_to_98: updates the value of a point n it points to to 98
  * @n: pointer to an int
  */
 void reset_to_98(int *n)
