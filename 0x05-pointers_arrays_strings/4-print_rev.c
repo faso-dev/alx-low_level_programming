a2a6623182aa2674bca8f812a6af11e742f03e9b
faso-dev <jeromeonadja28@gmail.com>
2022-10-25 16:23:40 -0700

    feat(pointers-arrays): task #4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/**
+ * print_rev - main entry
+ * prints a string in reverse, followed by a new line
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+	int j, len, i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	len = i;
+
+	for (j = len - 1; j >= 0; j--)
+		_putchar(s[j]);
+
+	_putchar('\n');
+}
