d954e17e1fb413b8cf668a4b8c2bd321b94457d0
faso-dev <jeromeonadja28@gmail.com>
2022-11-08 22:52:53 +0000

    fix(malloc-free): wong docs typo

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,10 +1,11 @@
+#include "main.h"
+
 /**
 * create_array - creates an array of chars, and initializes i
 * t with a specific char.
- * @size: size of the array
- * @c: char to initialize the array with
- * Return: pointer to the array, or NULL if it fails
- */
+* @size: size of the array
+* @c: char to initialize the array with
+* Return: pointer to the array, or NULL if it fails
 */
 char *create_array(unsigned int size, char c)
 {
@@ -12,10 +13,10 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
-		return ('\0');
+		return (NULL);
 	p = malloc(size * sizeof(char));
-	if (p == '\0')
-		return ('\0');
+	if (p == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		p[i] = c;
 	return (p);
