4da760c83becb5ba8bdbe18c58d85767ce2211ca
faso-dev <jeromeonadja28@gmail.com>
2022-11-19 15:19:32 +0000

    feat(preprocessor-struct): task #0 - #4 to #1 - #5

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,16 @@
+### Structure typedefs
+
+This directory contains examples of structure typedefs.
+
+#### Structure typedefs in C
+
+In C, a structure typedef is declared like this:
+
+```c
+typedef struct my_struct {
+    int a;
+    int b;
+} my_struct_t;
+```
+
+This creates a new type called `my_struct_t` that can be used to declare variables of type `my_struct`.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef dog
+#define dog
+#include <stdio.h>
+/**
+* struct dog - define a new type of struct dog
+* @name: name's dog
+* @age: age's dog
+* @owner: owner's dog
+*/
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
