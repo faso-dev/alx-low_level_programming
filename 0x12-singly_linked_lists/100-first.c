f329cb5f9a97acc871b1e323a64172325b124019
faso-dev <jeromeonadja28@gmail.com>
2022-11-27 11:10:27 +0000

    fix(linked-lists): semicolon

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,12 +4,12 @@
 * firstfunc - prints before the main function is executed.
 * Return: Always 0.
 */
-void firstfunc(void) __attribute__ ((constructor))
+void firstfunc(void) __attribute__ ((constructor));
 
 void firstfunc(void)
 {
 	char *m;
 
 	m = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
-	printf("%s", m)
+	printf("%s", m);
 }
