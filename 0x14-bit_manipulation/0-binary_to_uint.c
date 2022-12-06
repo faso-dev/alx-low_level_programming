2592827103d71217d04e18f9b4aa955d308d6cc7
faso-dev <jeromeonadja28@gmail.com>
2022-12-06 22:27:31 +0000

    enhance(0x14): improve task #0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,27 +5,27 @@
 * @b: a string of 0 and 1 chars
 * Return: the converted number or 0 if b is NULL or different from 0 or 1
 */
-unsigned int binary_to_uint(const char *b) {
-	// Check for NULL input
-	if (b == NULL) {
-		return 0;
-	}
+unsigned int binary_to_uint(const char *b)
+{
+	int length, i, add;
 
-	// Initialize the result
-	unsigned int result = 0;
+	add = 0;
 
-	// Iterate over the binary string and update the result
-	for (const char *p = b; *p != '\0'; p++) {
-		if (*p == '0') {
-			result = result << 1;
-		} else if (*p == '1') {
-			result = (result << 1) + 1;
-		} else {
-			// Invalid input, return 0
-			return 0;
+	if (b != NULL)
+	{
+		for (length = 0 ; b[length] != '\0' ; length++)
+		{
+			if (b[length] != 48 && b[length] != 49)
+				return (0);
+		}
+
+		for (i = 0, length-- ; length >= 0 ; length--)
+		{
+			add = add + ((b[length] - '0') << i);
+			i++;
 		}
 	}
-
-	// Return the result
-	return result;
+	else
+		return (0);
+	return (add);
 }
